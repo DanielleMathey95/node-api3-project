199ac3d7a799a25b29d2b67c4bd7c66ca0f1262e
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-09-09 22:16:29 -0700

    POST request complete

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,17 +1,21 @@
 const express = require("express");
-const { validateUserId, validateUser, validatePost } = require("../middleware/middleware");
+const {
+  validateUserId,
+  validateUser,
+  validatePost,
+} = require("../middleware/middleware");
 // You will need `users-model.js` and `posts-model.js` both
-const User = require('./users-model');
-const Post = require('../posts/posts-model');
+const User = require("./users-model");
+const Post = require("../posts/posts-model");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   User.get()
-    .then(users => {
-      res.json(users)
+    .then((users) => {
+      res.json(users);
     })
-    .catch(next)
+    .catch(next);
 });
 
 router.get("/:id", validateUserId, (req, res) => {
@@ -20,8 +24,11 @@ router.get("/:id", validateUserId, (req, res) => {
 });
 
 router.post("/", validateUser, (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+  User.insert({ name: req.name })
+    .then((newUser) => {
+      res.status(201).json(newUser);
+    })
+    .catch(next);
 });
 
 router.put("/:id", validateUserId, validateUser, (req, res) => {
@@ -40,7 +47,7 @@ router.get("/:id/posts", validateUserId, (req, res) => {
   // this needs a middleware to verify user id
 });
 
-router.post("/:id/posts", validateUserId,  validatePost,  (req, res) => {
+router.post("/:id/posts", validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
