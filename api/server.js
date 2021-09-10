3d0b2718a4df5f57a0d188c5a36331791795851b
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-09-09 21:36:57 -0700

    logger middleware complete

--- api/server.js
+++ api/server.js
@@ -1,11 +1,18 @@
-const express = require('express');
-
+const express = require("express");
+const {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+} = require("./middleware/middleware");
 const server = express();
 
-server.use(express.json)
+server.use(express.json);
+
+server.use(logger)
 // global middlewares and the user's router need to be connected here
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
